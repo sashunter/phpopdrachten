0a4e9e35376dec944ccaaa89027d24a0c858c408
Muhammad Mouchli <muhammad.mouchli@edu-kw1c.nl>
2020-02-05 14:09:41 +0100

    dat zijn mijn uitwerkingen

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: Muhammad Mouchli
+ * Date: 05-02-2020
+ * Time: 13:36
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="na">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="opdracht x">
+    <meta name="keywords" content="keyword 1, keyword 2">
+    <link href="cssstyle.css" type="text/css" rel="stylesheet">
+    <title>
+        PHP Professional
+    </title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+
+</body>
+</html>
---
+++ phpworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php print "pagina 1"; ?></title>
+    </head>
+    <body><h1>
+        <?php
+        echo "Hello Shab Utrecht";
+        ?>
+    </h1>
+
+    </body>
+
+
+
+
+
+
+</html>
