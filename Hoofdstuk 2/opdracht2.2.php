c268dd2e30ad58e0c4245ae34f2a497289df054c
Muhammad Mouchli <muhammad.mouchli@edu-kw1c.nl>
2020-02-12 09:16:52 +0100

    dat zijn mijn uitwerkingen voor 2.2

--- Hoofdstuk 2/opdracht2.2.php
+++ Hoofdstuk 2/opdracht2.2.php
@@ -1,10 +1,17 @@
 <?php
+//een string aangemaakt
     $text1 = "Hallo ";
+//een string aangemaakt
     $text2 ="toch makkelijke taal ";
+//een string aangemaakt
     $text3 = "wat is ";
+//een string aangemaakt
     $text4 = "PHP ";
+//een string aangemaakt
     $text5 = "nooit gedacht dat ";
+//een string aangemaakt
     $text6 = "de ingewikkelde installatie ";
+//een string aangemaakt
     $text7 = "Fijn toch? ";
 ?>
 <?php
@@ -31,11 +38,12 @@
         <h2>
             Uitwerkingen
         </h2>
+<!--        een h2 element-->
         <h2>
             Taak 2
         </h2>
         <p>
-            <?php echo $text1, $text3, $text4, $text2; ?>
+            <?php echo $text1. $text3. $text4  . $text2; ?>
         </p>
         <p>
             <?php echo $text3, $text6, $text7; ?>
@@ -47,8 +55,8 @@
             Hallo.
         </p>
         <p>
-            <?php echo $text7, "dat ", $text4, $text2, "is" ?><br>
-            <?php echo $text6, "is best moielijk, vind je niet?" ?>
+            <?php echo $text7 . "dat " . $text4 . $text2 . "is" ?><br>
+            <?php echo $text6 . "is best moielijk, vind je niet?" ?>
         </p>
 
 
